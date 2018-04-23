e27a8edd58a8052ff51ddda20a375f23a5cbd9e2
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-04-23 13:49:55 +0200

    substring with regex

--- graph3.hpp
+++ graph3.hpp
@@ -5,19 +5,25 @@
 #include <stack>
 
 template<class VDATA, class EDATA>
-class Graph {
+class Graph
+{
 
 public:
 	class Edge;
-	class Vertex {
+
+    class Vertex
+    {
 	private:
 		VDATA value;
 		std::vector<Edge*> edges;
+
 	public:
-		class NeighborEdgeIterator {
+        class NeighborEdgeIterator
+        {
 		private:
-			int pos;
+            unsigned int pos;
 			std::vector<Edge*>* np;
+
 		public:
 			NeighborEdgeIterator() { pos = 0; }
 			bool has_next() { return pos < np->size(); }
@@ -25,7 +31,9 @@ public:
 			Edge* current() { return has_next() ? (*np)[pos] : 0; }
 			void set_np(std::vector<Edge*>* np) { this->np = np; }
 		};
-		NeighborEdgeIterator neighbor_iterator() {
+
+        NeighborEdgeIterator neighbor_iterator()
+        {
 			NeighborEdgeIterator ni;
 			ni.set_np(&edges);
 			return ni;
@@ -38,13 +46,15 @@ public:
 		Edge* get_edge(int i) { return edges[i]; }
 	};
 
-	class Edge {
+    class Edge
+    {
 	private:
 		EDATA weight;
 		Vertex* source;
 		Vertex* destination;
 		bool directed;
 	public:
+
 		Edge(EDATA weight, Vertex* source, Vertex* destination, bool directed = false)
 		{
 			this->weight = weight;
@@ -52,14 +62,22 @@ public:
 			this->destination = destination;
 			this->directed = directed;
 		}
+
 		EDATA get_weight() { return weight; }
 		Vertex* get_source() { return source; }
 		Vertex* get_destination() { return destination; }
-		Vertex* get_destination(Vertex* source) {
+
+        Vertex* get_destination(Vertex* source)
+        {
 			if (source == this->source)
+            {
 				return destination;
+            }
 			if (!directed && source == this->destination)
-				return this->source;
+            {
+                return this->source;
+            }
+
 			return 0;
 		}
 	};
@@ -71,10 +89,12 @@ private:
 
 public:
 
-	class VertexIterator {
+    class VertexIterator
+    {
 	private:
-		int pos;
+        unsigned int pos;
 		std::vector<Vertex*>* vp;
+
 	public:
 		VertexIterator() { pos = 0; }
 		bool has_next() { return pos < vp->size(); }
@@ -82,6 +102,7 @@ public:
 		Vertex* current() { return has_next() ? (*vp)[pos] : 0; }
 		void set_vp(std::vector<Vertex*>* vp) { this->vp = vp; }
 	};
+
 	VertexIterator vertex_iterator()
 	{
 		VertexIterator vi;
@@ -89,10 +110,12 @@ public:
 		return vi;
 	}
 
-	class EdgeIterator {
+    class EdgeIterator
+    {
 	private:
 		int pos;
 		std::vector<Edge*>* ep;
+
 	public:
 		EdgeIterator() { pos = 0; this->ep = ep; }
 		bool has_next() { return pos < ep->size(); }
@@ -100,6 +123,7 @@ public:
 		Edge* current() { return has_next() ?(*ep)[pos] : 0; }
 		void set_ep(std::vector<Edge*>* ep) { this->ep = ep; }
 	};
+
 	VertexIterator edge_iterator()
 	{
 		EdgeIterator ei;
@@ -107,16 +131,20 @@ public:
 		return ei;
 	}
 
-	class DepthFirstIterator {
+    class DepthFirstIterator
+    {
 	private:
-		struct StackElement {
+        struct StackElement
+        {
 			Vertex* vertex;
 			int pos;
 		};
+
 		std::vector<Vertex*>* vp;
 		std::set<Vertex*> visited;
 		std::stack<StackElement> stack;
 		Vertex* following;
+
 	public:
 		DepthFirstIterator(Vertex* begin)
 		{
@@ -126,27 +154,45 @@ public:
 			se.pos = 0;
 			stack.push(se);
 		}
+
 		void set_vp(std::vector<Vertex*>* vp) { this->vp = vp; }
-		bool has_next() { return (following != 0); }
-		void next() {
-			if (following == 0) return;
+        bool has_next() { return following != 0; }
+
+        void next()
+        {
+            if (following == 0)
+            {
+                return;
+            }
 
 			following = 0;
-			if (stack.size() == 0) return;
+
+            if (stack.size() == 0)
+            {
+                return;
+            }
 
 			StackElement se = stack.top();
 			stack.pop();
-			while (following == 0 && se.pos < se.vertex->get_degree()) {
+
+            while (following == 0 && se.pos < se.vertex->get_degree())
+            {
 				Edge* edge = se.vertex->get_edge(se.pos);
 				Vertex* next = edge->get_destination(se.vertex);
-				if (visited.count(next) == 0) {
+                if (visited.count(next) == 0)
+                {
 					following = next;
 				}
 				se.pos ++;
 			}
+
 			if (se.pos < se.vertex->get_degree())
+            {
 				stack.push(se);
-			if (following) {
+            }
+
+            if (following)
+            {
 				se.vertex = following;
 				se.pos = 0;
 				stack.push(se);
@@ -155,7 +201,10 @@ public:
 		Vertex* current()
 		{
 			if (following)
+            {
 				visited.insert(following);
+            }
+
 			return following;
 		}
 		
@@ -164,13 +213,14 @@ public:
 	{
 		DepthFirstIterator dfi(begin);
 		dfi.set_vp(&vertices);
+
 		return dfi;
 	}
 
 	Graph(bool dir)
 	{
 		directed = dir;
-	}
+    }
 
 	int num_vertices() { return vertices.size(); }
 	bool is_directed() { return directed; }
@@ -184,12 +234,17 @@ public:
 
 	Edge* add_edge(EDATA weight, Vertex* source, Vertex* destination)
 	{
-		if (source && destination) {
+        if (source && destination)
+        {
 			Edge* ret = new Edge(weight, source, destination, is_directed());
 			edges.push_back(ret);
 			source->add_neighbor(ret);
+
 			if (!directed)
+            {
 				destination->add_neighbor(ret);
+            }
+
 			return ret;
 		}
 		return 0;
--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <regex>
 
 #include "graph3.hpp"
 
@@ -8,11 +11,15 @@ void rempli(Graph<int,int>& g)
     std::vector<Graph<int,int>::Edge*> et;
 
     for (int i=10; i<=50; i+=10)
+    {
         vt.push_back(g.add_vertex(i));
+    }
 
     int src[] = {0, 0, 1, 1, 2, 2, 3, -1};
     int dst[] = {1, 2, 2, 3, 3, 4, 4};
-    for (int i=0; src[i] >= 0; i++) {
+
+    for (int i=0; src[i] >= 0; i++)
+    {
         g.add_edge(i+1, vt[src[i]], vt[dst[i]]);
     }
 }
@@ -20,10 +27,14 @@ void rempli(Graph<int,int>& g)
 void affiche(Graph<int,int>& g)
 {
     std::string lien = g.is_directed() ? " -> " : " -- ";
-    for (Graph<int,int>::VertexIterator vi = g.vertex_iterator(); vi.has_next(); vi.next()) {
+
+    for (Graph<int,int>::VertexIterator vi = g.vertex_iterator(); vi.has_next(); vi.next())
+    {
         Graph<int,int>::Vertex* curr = vi.current();
         std::cout << "vertex [" << curr->get_value() << "]" << std::endl;
-        for (Graph<int,int>::Vertex::NeighborEdgeIterator ni = curr->neighbor_iterator(); ni.has_next(); ni.next()) {
+
+        for (Graph<int,int>::Vertex::NeighborEdgeIterator ni = curr->neighbor_iterator(); ni.has_next(); ni.next())
+        {
             Graph<int,int>::Edge* neighbor_edge = ni.current();
             Graph<int,int>::Vertex* neighbor = neighbor_edge->get_destination(curr);
             std::cout << "  edge " << curr->get_value() << lien << neighbor->get_value() << " [" << neighbor_edge->get_weight() << "] " << std::endl;
@@ -34,24 +45,79 @@ void affiche(Graph<int,int>& g)
 void visite_profondeur(Graph<int,int>& g)
 {
     Graph<int,int>::Vertex* v = g.get_vertex(0);
-    for (Graph<int,int>::DepthFirstIterator dfi = g.depth_first_iterator(v); dfi.has_next(); dfi.next()) {
+
+    for (Graph<int,int>::DepthFirstIterator dfi = g.depth_first_iterator(v); dfi.has_next(); dfi.next())
+    {
         std::cout << "visit " << dfi.current()->get_value() << std::endl;
     }
 }
 
+Graph<std::string, int> graph_from_file(std::string filename)
+{
+    Graph<std::string,int> g(false);
+
+    std::ifstream file;
+    std::set<std::string> set_firstnames;
+
+    try
+    {
+        file.open(filename);
+        std::string line;
+
+        const int NUM_EMPTY_LINES = 2;
+
+        for(int i = 0; i < NUM_EMPTY_LINES; i++)
+        {
+            std::getline(file, line);
+        }
+
+        while(std::getline(file, line))
+        {
+            std::regex re("(\\w+) -- (\\w+) \\[label=(\\d+)\\]");
+            std::smatch match;
+            std::string result;
+
+            if (std::regex_search(line, match, re) && match.size() > 1)
+            {
+                result = match.str(1);
+                std::cout << result << std::endl;
+                result = match.str(2);
+                std::cout << result << std::endl;
+                result = match.str(3);
+                std::cout << result << std::endl;
+            }
+            else
+            {
+                result = std::string("");
+            }
+
+        }
+
+        file.close();
+    }
+    catch (std::ios_base::failure& fail)
+    {
+        std::cout << fail.what() << std::endl;
+    }
+
+    return g;
+}
+
 int main()
 {
-    Graph<int,int> g1(true);
-    rempli(g1);
-    std::cout << "G1 **********\n";
-    affiche(g1);
-    std::cout << "dpi G1 **********\n";
-    visite_profondeur(g1);
+//    Graph<int,int> g1(true);
+//    rempli(g1);
+//    std::cout << "G1 **********\n";
+//    affiche(g1);
+//    std::cout << "dpi G1 **********\n";
+//    visite_profondeur(g1);
 
-    Graph<int,int> g2(false);
-    rempli(g2);
-    std::cout << "G2 **********\n";
-    affiche(g2);
-    std::cout << "dpi G2 **********\n";
-    visite_profondeur(g2);
+//    Graph<int,int> g2(false);
+//    rempli(g2);
+//    std::cout << "G2 **********\n";
+//    affiche(g2);
+//    std::cout << "dpi G2 **********\n";
+//    visite_profondeur(g2);
+
+    graph_from_file("test.dot");
 }
